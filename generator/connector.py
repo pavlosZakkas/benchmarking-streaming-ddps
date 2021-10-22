1358966ecd273a8c69d1bcc68d555d58afad8823
pavlosZakkas <pavloszk93@gmail.com>
2021-10-23 01:10:47 +0200

    Add socket connector
    - Opens a socket at the given host and port
    - Accepts and returns the connection socket

---
+++ generator/connector.py
@@ -0,0 +1,15 @@
+import socket
+
+class SocketConnectionException(Exception):
+  pass
+
+def wait_for_connection_to(host, port):
+  try:
+    stream_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    stream_socket.bind((host, port))
+    stream_socket.listen()
+    connection, addr = stream_socket.accept()
+    return connection
+
+  except Exception as e:
+    raise SocketConnectionException('Failed to set up connection with the streaming system', e)
---
+++ generator/tests/connector_spec.py
@@ -0,0 +1,39 @@
+import unittest
+import mock
+import pytest
+
+from generator import connector
+from generator.connector import SocketConnectionException
+
+class ConnectorSpec(unittest.TestCase):
+  HOST = 'a_host'
+  PORT = '1234'
+
+  def setUp(self) -> None:
+    self.SOCKET_MOCK = mock.Mock()
+    self.CONNECTION_MOCK = mock.Mock()
+
+  def setup_mock(self, socket_mock):
+    socket_mock.return_value = self.SOCKET_MOCK
+    self.SOCKET_MOCK.bind.return_value = mock.Mock()
+    self.SOCKET_MOCK.listen.return_value = mock.Mock()
+    self.SOCKET_MOCK.accept.return_value = (self.CONNECTION_MOCK, mock.Mock())
+
+  def setup_failing_mock(self, socket_mock):
+    socket_mock.return_value = self.SOCKET_MOCK
+    self.SOCKET_MOCK.bind.side_effect = OSError('Failed to connect')
+
+  @mock.patch('socket.socket')
+  def should_raise_an_exception_when_socket_fails_to_connect(self, socket_mock):
+    self.setup_failing_mock(socket_mock)
+    with pytest.raises(SocketConnectionException):
+      connector.wait_for_connection_to(self.HOST, self.PORT)
+
+  @mock.patch('socket.socket')
+  def should_connect_to_the_given_host_and_port(self, socket_mock):
+    self.setup_mock(socket_mock)
+
+    connection = connector.wait_for_connection_to(self.HOST, self.PORT)
+
+    self.SOCKET_MOCK.bind.assert_called_once_with((self.HOST, self.PORT))
+    self.assertEqual(connection, self.CONNECTION_MOCK)
