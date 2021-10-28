95c88c30ba1cb4293a24f825667d1842202f87ad
pavlosZakkas <pavloszk93@gmail.com>
2021-10-28 02:09:06 +0200

    Add environment variables for running spark on DAS5 and locally

---
+++ system_under_test/spark/deployment/das5_env.py
@@ -0,0 +1,12 @@
+import pathlib
+
+PATH_TO_SPARK_MODULE = pathlib.Path(__file__).parent.parent.resolve()
+SPARK_HOME = '/home/ddps2107/Soft/spark-3.2.0-bin-hadoop3.2'
+STREAMER_HOST = 'localhost'
+STREAMER_POST = '9999'
+
+SPARK_MASTER_PORT = 7077
+SPARK_MASTER_WEBUI_PORT = 8088
+JAVA_HOME = '/usr'
+SPARK_WORKER_CORES = 16
+SPARK_WORKER_MEMORY = '16g'
