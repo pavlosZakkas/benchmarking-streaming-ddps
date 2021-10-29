8ccde591e99ec9af88de99f5129cb583b6c7d5be
pavlosZakkas <pavloszk93@gmail.com>
2021-10-29 23:38:11 +0200

    Reformat spark deployment scripts and apply last changes

--- system_under_test/spark/deployment/stop_cluster.py
+++ system_under_test/spark/deployment/stop_cluster.py
@@ -1,13 +1,15 @@
 import os
 
 if os.environ.get('DEPLOY_ENV') == 'LOCAL':
-  import local_env as env
+    import local_env as env
 else:
-  import das5_env as env
+    import das5_env as env
+
 
 def stop_cluster():
-  os.chdir(f'{env.SPARK_HOME}')
-  os.system(f'./sbin/stop-all.sh')
+    os.chdir(f'{env.SPARK_HOME}')
+    os.system(f'./sbin/stop-all.sh')
+
 
 if __name__ == "__main__":
-  stop_cluster()
+    stop_cluster()
